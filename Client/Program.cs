3f0d6838d897473283ef884b9b0c0a5a010ce03a
sadovrs <sadovrs@ARMCSR17>
2022-10-07 09:38:31 +0300

    добавлен лог временем подключения к серверу

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Runtime.Remoting;
 using System.Runtime.Remoting.Channels;
 using System.Runtime.Remoting.Channels.Tcp;
@@ -12,12 +13,19 @@ namespace Client
             TcpChannel tcpCanC = new TcpChannel();
             ChannelServices.RegisterChannel(tcpCanC, false);
 
-            Server_obj.ServerObj obj1=(Server_obj.ServerObj)Activator.GetObject(typeof(Server_obj.ServerObj),
-                "tcp://localhost:11000/met1");
+            Server_obj.ServerObj obj1 = (Server_obj.ServerObj)Activator.GetObject(typeof(Server_obj.ServerObj), "tcp://localhost:11000/met1");
             tcpCanC.StopListening(obj1);
-            Udal_obj.Udal_obj obj2= (Udal_obj.Udal_obj)Activator.GetObject(typeof(Udal_obj.Udal_obj),
-                "tcp://localhost:11000/met2");
+
+            Udal_obj.Udal_obj obj2 = (Udal_obj.Udal_obj)Activator.GetObject(typeof(Udal_obj.Udal_obj), "tcp://localhost:11000/met2");
             tcpCanC.StartListening(obj2);
+
+            if (ChannelServices.GetChannel(tcpCanC.ChannelName) != null)
+            {
+                using (StreamWriter sw = new StreamWriter($"C:\\Users\\{Environment.UserName}\\Documents\\log.txt", true))
+                {
+                    sw.WriteLine(String.Format($"* {DateTime.Now} - Соединение с сервером установлено"));
+                } 
+            }
 
             Console.WriteLine("Введите целое число:");
             Console.WriteLine($"Результат вычислений: {obj1.ServerMethod(Console.ReadLine())}");
